ba96e9c0854d6e1da2ce02493808bb520d1b5ddc
jyoti <jyotisankardas27@gmail.com>
2020-05-03 20:55:00 +0530

    fiften

--- src/Fiften/Remove_Specific_Character.java
+++ src/Fiften/Remove_Specific_Character.java
@@ -3,8 +3,8 @@ package Fiften;
 public class Remove_Specific_Character {
 	
 	public static void main(String[] args) {
-		String s="rama is a good boy";
-		System.out.println(s.);
+		String s="Learning never stops";
+		System.out.println(s.replaceAll("[estp]", ""));
 	}
 
 }
